8efcbce4d9ab7f266aa26dea766113ced721ee6b
Pedro Hurtado <pedrohurt@gmail.com>
2024-04-25 13:39:48 +0200

    django

---
+++ members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def members(request):
+    print(request.GET.get('name'))
+    return HttpResponse("Hello world!")
+
+def member(request,id):
+    return HttpResponse("Hello world!" + str(id))
