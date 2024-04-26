f96593eca7a83312b2cc0c55dedc76830de1ff0c
Pedro Hurtado <pedrohurt@gmail.com>
2024-04-26 10:07:39 +0200

    templates django

--- members/views.py
+++ members/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-
+from django.template import loader
 # Create your views here.
 
 from django.http import HttpResponse
 
 def members(request):
     print(request.GET)
-    return HttpResponse("Hello world!")
+    template = loader.get_template('block.html') 
+    context = {"blog_entries":[
+        {
+            "title":"titulo1",
+            "body":"body1"
+        },
+        {
+            "title":"titulo2",
+            "body":"body2"
+        }
+    ]}
+    
+    return HttpResponse(template.render(context=context))
 
 def member(request,id):
     return HttpResponse("Hello world!" + str(id))
